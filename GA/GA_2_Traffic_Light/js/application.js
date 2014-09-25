2720d597bf1a0fd6f60cffa107b496249c8d57f4
Jorge Gutierrez <jorgeforpresidente@gmail.com>
2014-09-24 21:30:20 -0400

    Traffic Light Edits
    add  edit css/jv

--- GA/GA_2_Traffic_Light/js/application.js
+++ GA/GA_2_Traffic_Light/js/application.js
@@ -1,1 +1,15 @@
-alert("hello world")
+function buttonClick(lightOn){
+  
+  var greenLight =document.getElementById('green'); 
+  var yellowLight = document.getElementById('yellow'); 
+  var redLight = document.getElementById('red');
+  
+  greenLight.style.backgroundColor = "black"; 
+  yellowLight.style.backgroundColor = "black";
+  redLight.style.backgroundColor = "black";
+  
+  var light = document.getElementById(lightOn);
+  light.style.backgroundColor = lightOn;  
+}
+
+ 
