bc25298e05c3ccdef351aeba1366df13aeea1339
Jorge Gutierrez <jorgeforpresidente@gmail.com>
2014-10-02 18:31:10 -0400

    Slideshow
    Edits

--- GA/GA_3_Slideshow/js/application.js
+++ GA/GA_3_Slideshow/js/application.js
@@ -1,1 +1,26 @@
-alert("I work!");
+var images = ("img/photo_1.jpg", "img/photo_2.jpg", "img/photo_3.jpg", "img/photo_4.jpg")
+var currentImage = 0;
+
+function changePicture(whichNum){
+var img = document.getElementById('mainImage')
+img.setAttribute('src', images[whichNum]);
+currentImage=whichNum;
+}
+
+function nextImage(){
+currentImage=;
+if (currentImage=images.length){
+	currentImage=0;
+}
+
+var img = document.getElementById('mainImage');
+img.setAttribute('src', images[currentImage]);
+
+}
+
+function prevImage(){
+currentImage=currentImage=1;
+var img = document.getElementById('mainImage')
+
+img.setAttribute('src', images[whichNum]);
+}
